79f9e7f949892e310db456517205e5d15c69e6a0
tatiana faramarzi <tfaramarzi@gmail.com>
2019-09-04 10:50:03 -0700

    implemented a suppliers sub-router

---
+++ suppliers/suppliers-router.js
@@ -0,0 +1,22 @@
+//Exercise: implement suppliers sub-router
+const express = request('express');
+
+const router = express.Router();
+
+router.use(express.json());
+
+//implement a GET to /suppliers, respond with a message
+router.get('/', (req, res) => {
+    res.send('you got to /suppliers/')
+})
+
+//implement a GET to /suppliers/name that returns the name sent
+router.get('/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${name}`)
+})
+
+module.export = router;
+
+
+
